a1cff9db42b1a701cb0f3d863bf0f7bea2436628
OHSeungYeon <osyddwu079821@gmail.com>
2023-09-20 12:22:25 +0900

    맵페이지1 div 완료 _ 0920

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Firstpage from "./pages/firstpage";
 import "./index.css";
-
+import Mappage1 from "./pages/mappage1";
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Firstpage />} />
+          <Route path="/Mappage1" element={<Mappage1 />} />
         </Routes>
       </div>
     </BrowserRouter>
