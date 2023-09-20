0099b7f400d45c0fbd17ea5f5422c2862476a0f0
OHSeungYeon <osyddwu079821@gmail.com>
2023-09-20 13:29:17 +0900

    맵페이지3 div 완료 _ 0920

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Firstpage from "./pages/firstpage";
 import "./index.css";
 import Mappage1 from "./pages/mappage1";
+import Mappage3 from "./pages/mappage3";
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
           <Route path="/Mappage1" element={<Mappage1 />} />
+          <Route path="/Mappage3" element={<Mappage3 />} />
         </Routes>
       </div>
     </BrowserRouter>
